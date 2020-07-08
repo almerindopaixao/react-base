f8b21f1c01736f710118fd20a2088e4755734e3c
Almerindo Paixão <almerindopaixao@gmail.com>
2020-07-08 11:29:45 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Header from './components/Header';
 import Login from './pages/Login';
 import GlobalStyle from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
     </>
