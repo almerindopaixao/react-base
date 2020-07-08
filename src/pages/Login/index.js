aa2ff74fcf79a8fee5de12f178c56664b5782677
Almerindo Paixão <almerindopaixao@gmail.com>
2020-07-08 10:42:19 -0300

    styled components

---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title isRed={false}>
+        Login
+        <small>OIE</small>
+      </Title>
+      <Paragrafo>Lorem ipsu dolo si amerere.</Paragrafo>
+    </Container>
+  );
+}
